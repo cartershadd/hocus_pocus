d9dea14360253ee4960197748eae2585e1de6513
Nathan Burnham <nburn42@gmail.com>
2019-11-09 15:00:29 -0500

    Added main loop

--- main.js
+++ main.js
@@ -3,9 +3,28 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext('2d');
 
-console.log("test");
+console.log("Engage");
 
-// do cool things with the context
-ctx.font = '40pt Calibri';
-ctx.fillStyle = 'blue';
-ctx.fillText('Hello World!', 100, 100);
+
+function update(progress) {
+    // Move characters
+}
+
+function draw() {
+    ctx.font = '40pt Calibri';
+    ctx.fillStyle = 'blue';
+    ctx.fillText('Hello World!', 100, 100);
+}
+
+// Main game loop run forever
+function loop(timestamp) {
+    var progress = timestamp - lastRender;
+
+    update(progress);
+    draw();
+
+    lastRender = timestamp;
+    window.requestAnimationFrame(loop)
+}
+var lastRender = 0;
+window.requestAnimationFrame(loop);
