8552894ead8f2f002a4687006d492f5916f64dd9
Nathan Burnham <nburn42@gmail.com>
2019-11-09 14:41:57 -0500

    Added initial javasctipt file

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.canvas.getContext('2d');
+
+
+// do cool things with the context
+context.font = '40pt Calibri';
+context.fillStyle = 'blue';
+context.fillText('Hello World!', 150, 100);
